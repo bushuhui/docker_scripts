0b00298ec6deee2ce9fc25c0e29a360ee1291d49
Shuhui Bu <bushuhui@gmail.com>
2018-07-30 21:12:06 +0800

    Improve saveImage loadImage

--- docker_loadImage.sh
+++ docker_loadImage.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 # restore odm_dev image
-docker load < odm_dev.tar
+#docker load < odm_dev.tar
 
+show_usage()
+{
+cat << EOF
+Usage: 
+    ${0##*/} [docker image]
+
+Load saved docker image file to docker system
+
+EOF
+}
+
+
+if [[ $# -gt 0 ]]; then
+    echo ""; echo ">>> load file: $1 ..."
+    docker load < $1
+else
+    show_usage
+    exit -1
+fi
 
--- docker_saveImage.sh
+++ docker_saveImage.sh
@@ -1,7 +1,87 @@
 #!/bin/bash
 
 # save odm_dev image
-docker commit odm odm_dev
-docker save odm_dev -o odm_dev.tar
+#docker commit odm odm_dev
+#docker save odm_dev -o odm_dev.tar
+
+docker_container=""
+docker_image=""
 
 
+save_docker_container()
+{
+    echo ""; echo ">>> commit [$docker_container] -> [$docker_image] ..."
+    docker commit "$docker_container" "$docker_image"
+    
+    echo ""; echo ">>> save [$docker_image] -> local file: ${docker_image}.tar ..."
+    docker save "$docker_image" -o "${docker_image}.tar"
+}
+
+
+save_docker_image()
+{
+    echo ""; echo ">>> save [$docker_image] -> local file: ${docker_image}.tar ..."
+    docker save "$docker_image" -o "${docker_image}.tar"
+}
+
+
+show_usage()
+{
+cat << EOF
+Usage: 
+    ${0##*/} [-c docker_container] [-i docker_image]
+
+Save docker container or image to local file
+
+  Opts:
+    -i, --image         Required. Set the name of the image.
+    -c, --container     Required. Set the container name
+    
+EOF
+}
+
+
+# parse input arguments
+params="$(getopt -o hi:c: -l image:,container: --name "$0" -- "$@")"
+eval set -- "$params"
+act=""
+
+while [[ $# -gt 0 ]] ; do
+    case $1 in
+        -h|-\?|--help)
+            show_usage
+            exit 0
+            ;;
+            
+            
+        -i|--image)
+            if [ -n "$2" ]; then
+                docker_image=$2
+                shift
+            fi
+            ;;
+        -c|--container)
+            if [ -n "$2" ]; then
+                docker_container=$2
+                
+                if [[ ! -n "$docker_image" ]]; then
+                    docker_image="${docker_container}_image"
+                fi
+                
+                shift
+            fi
+            ;;
+    esac
+    shift
+done
+
+if [[ -n "$docker_container" ]]; then
+    save_docker_container
+else
+    if [[ -n "$docker_image" ]]; then
+        save_docker_image
+    else
+        show_usage
+        exit 0
+    fi
+fi
